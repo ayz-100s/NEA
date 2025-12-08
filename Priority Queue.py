48a311a87fb15c4699c5e1f00f67bd2b767e0754
ayz-100s <ayaanw4zir@gmail.com>
2025-12-08 22:55:35 +0000

    Testing commit

--- Priority Queue.py
+++ Priority Queue.py
@@ -9,18 +9,18 @@ class TopicNode:
         self.priority_queue = []  #(next_review_time, question_id)
         self.completed = False  #whether topic has been answered at least once
 
-    def add_subtopic(self, subtopic_node):
+    def AddSubtopic(self, subtopic_node):
         self.subtopics.append(subtopic_node)
 
-    def add_question(self, question_id, next_review_time):
+    def AddQuestion(self, question_id, next_review_time):
         heapq.heappush(self.priority_queue, (next_review_time, question_id))
 
-    def pop_next_question(self):
+    def PopQuestion(self):
         if self.priority_queue:
             return heapq.heappop(self.priority_queue)
         return None
 
-    def is_soft_complete(self):
+    def Completed(self):
         if self.priority_queue:
             return False
         for sub in self.subtopics:
@@ -28,7 +28,7 @@ class TopicNode:
                 return False
         return True
 
-    def reschedule_completed_questions(self, factor=2):
+    def CompletedTopics(self, factor=2):
         new_queue = []
         for next_time, qid in self.priority_queue:
             new_time = datetime.now() + (next_time - datetime.now()) * factor
